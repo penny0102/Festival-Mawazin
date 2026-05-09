50822263413bbab7b96f7c1761c5959e04133709
miraimo <lellymohammed2006@gmail.com>
2026-05-10 00:23:41 +0100

    update code

---
+++ src/pages/Passeport.jsx
@@ -0,0 +1,9 @@
+const Passeport = () => {
+    return(
+        <>
+            <div >
+                <h1></h1>
+            </div>
+        </>
+    )
+}
--- src/pages/Programes.jsx
+++ src/pages/Programes.jsx
@@ -2,10 +2,12 @@ import num from "../data/data";
 import { Cards } from "../components/Card";
 
 function setStorage(id) {
-  const storage = JSON.parse(localStorage.getItem("fav")) || [];
-
-  
+  let storage = JSON.parse(localStorage.getItem("fav")) || [];
   storage.push(id);
+  
+  let filterdStorage = new Set(storage)
+  storage = Array.from(filterdStorage)
+  
   localStorage.setItem("fav", JSON.stringify(storage));
 }
 
@@ -13,8 +15,6 @@ export function Programes() {
   
   return (
     <>
-    
-      
     <ul className="flex gap-15 justify-center items-center">
       <li className="bg-blue-300 py-3 px-3 border-2 border-black rounded-2xl"><a href=""></a>10</li>
       <li className="bg-blue-300 py-3 px-3 border-2 border-black rounded-2xl"><a href=""></a>20</li>
@@ -22,9 +22,11 @@ export function Programes() {
       <li className="bg-blue-300 py-3 px-3 border-2 border-black rounded-2xl"><a href=""></a>40</li>
       <li className="bg-blue-300 py-3 px-3 border-2 border-black rounded-2xl"><a href=""></a>50</li>
     </ul>
+
     <div className="flex justify-center mt-15 text-2xl bg-amber-50 w-50 ml-145 rounded-xl font-mono shadow-md shadow-black border-2 border-black">
       <h1>select artist</h1>
     </div>
+    
     <div className="flex gap-30 flex-wrap  mt-20 ml-55 font-mono">
       {num.map((data) => (
         <Cards
